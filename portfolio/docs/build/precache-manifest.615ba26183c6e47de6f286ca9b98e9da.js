d47125b2230637bfadd46e95c693da97e0863df3
Sonatai <gamedevsonatai@yahoo.com>
2020-05-20 23:51:01 +0200

    my own home page �

---
+++ portfolio/docs/build/precache-manifest.615ba26183c6e47de6f286ca9b98e9da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9429a76b0394abc97fc1540a114a333c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "45b77391c677d65f29cd",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3e416789baba60e007de",
+    "url": "/portfolio/static/js/2.a617a02f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.a617a02f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45b77391c677d65f29cd",
+    "url": "/portfolio/static/js/main.2c36c2ae.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  }
+]);
---
+++ portfolio/src/Pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage : React.FC = () => {
+    return(
+        <>
+            <h1>🎉Hier entsteht ein Portfolio😁</h1>
+            <pre>Ist aber noch in Bearbeitung 🤢🤢🤢🤢</pre>
+        </>
+    );
+}
+
+export default HomePage;
