83090ab26b19c5bd38dcb5ab9c97c03650c146af
Sonatai <gamedevsonatai@yahoo.com>
2020-05-21 00:41:26 +0200

    Header + Footer§

---
+++ src/Component/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+	return (
+		<>
+			<pre>Link zum Github | Link zum Itchio</pre>
+			<pre>© Copyright 2020 Jenny Gebauer</pre>
+		</>
+	);
+};
+
+export default Footer;
---
+++ src/Component/Header.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+	return <h3>Home | Game Projects | Software Projects | Jams | Resume</h3>;
+};
+export default Header;
